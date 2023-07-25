e66f8186c9057a611f9dc2d17bd3c71da4eb5eeb
Philipp Wagner <pwag86@googlemail.com>
2023-07-25 09:16:56 +0200

    Add Initial OData Service Request

--- BlazorDataGridExample/BlazorDataGridExample/Program.cs
+++ BlazorDataGridExample/BlazorDataGridExample/Program.cs
@@ -2,12 +2,23 @@ using BlazorDataGridExample;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Fast.Components.FluentUI;
+using Microsoft.OData.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// OData
+builder.Services.AddScoped(sp =>
+{
+    var dataServiceContext = new WideWorldImportersService.Container(new Uri("http://localhost:5000/odata"));
+
+    dataServiceContext.HttpRequestTransportMode = HttpRequestTransportMode.HttpClient;
+
+    return dataServiceContext;
+});
 
 
 builder.Services.AddFluentUIComponents();
