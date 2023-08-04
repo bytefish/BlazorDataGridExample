bab43f2ee7ef584e08d3fc32ccbefbfc0d7ad0e8
Philipp Wagner <pwag86@googlemail.com>
2023-08-04 10:57:16 +0200

    Re-Added Enum Translation

--- BlazorDataGridExample/BlazorDataGridExample/Components/BooleanFilter.razor.cs
+++ BlazorDataGridExample/BlazorDataGridExample/Components/BooleanFilter.razor.cs
@@ -16,25 +16,91 @@ namespace BlazorDataGridExample.Components
         /// </summary>
         [Parameter]
         public required FilterState FilterState { get; set; }
-
         /// <summary>
-        /// Filter Options available for the BooleanFilter.
+        /// Filter Options available for the DateTimeFilter.
         /// </summary>
         private readonly FilterOperatorEnum[] filterOperatorOptions = new[]
         {
             FilterOperatorEnum.IsNull,
             FilterOperatorEnum.IsNotNull,
+            FilterOperatorEnum.All,
             FilterOperatorEnum.Yes,
             FilterOperatorEnum.No,
         };
 
-        string? filterValue;
 
-        FilterOperatorEnum filterOperator;
+        protected FilterOperatorEnum _filterOperator { get; set; }
+
+        protected DateTime? _startDateTime { get; set; }
+
+        protected DateTime? _endDateTime { get; set; }
 
         protected override void OnInitialized()
         {
             base.OnInitialized();
+
+            SetFilterValues();
+        }
+
+        private bool IsStartDateTimeDisabled()
+        {
+            return _filterOperator == FilterOperatorEnum.None
+                || _filterOperator == FilterOperatorEnum.IsNull
+                || _filterOperator == FilterOperatorEnum.IsNotNull;
+        }
+
+        private bool IsEndDateTimeDisabled()
+        {
+            return (_filterOperator != FilterOperatorEnum.BetweenInclusive && _filterOperator != FilterOperatorEnum.BetweenExclusive);
+        }
+
+        private void SetFilterValues()
+        {
+            if (!FilterState.Filters.TryGetValue(PropertyName, out var filterDescriptor))
+            {
+                _filterOperator = FilterOperatorEnum.None;
+                _startDateTime = null;
+                _endDateTime = null;
+
+                return;
+            }
+
+            var dateTimeFilterDescriptor = filterDescriptor as DateTimeFilterDescriptor;
+
+            if (dateTimeFilterDescriptor == null)
+            {
+                _filterOperator = FilterOperatorEnum.None;
+                _startDateTime = null;
+                _endDateTime = null;
+
+                return;
+            }
+
+            _filterOperator = dateTimeFilterDescriptor.FilterOperator;
+            _startDateTime = dateTimeFilterDescriptor.StartDateTime?.DateTime;
+            _startDateTime = dateTimeFilterDescriptor.EndDateTime?.DateTime;
+        }
+
+        protected virtual Task ApplyFilterAsync()
+        {
+            var numericFilter = new DateTimeFilterDescriptor
+            {
+                PropertyName = PropertyName,
+                FilterOperator = _filterOperator,
+                StartDateTime = _startDateTime,
+                EndDateTime = _endDateTime,
+            };
+
+            return FilterState.AddFilterAsync(numericFilter);
+        }
+
+        protected virtual async Task RemoveFilterAsync()
+        {
+            _filterOperator = FilterOperatorEnum.None;
+            _startDateTime = null;
+            _endDateTime = null;
+
+            await FilterState.RemoveFilterAsync(PropertyName);
         }
     }
 }
